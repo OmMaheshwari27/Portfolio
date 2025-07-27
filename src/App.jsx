a303b9f781b7d84e949e929bc3674b53e74dd8d2
OmMaheshwari27 <oom12345609@gmail.com>
2025-07-27 20:05:36 +0530

    added css and dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,25 @@
-import { useState } from 'react'
-
 import './App.css'
+import { ThemeProvider } from './contexts/ThemeContext';
+import Navbar from './components/NavBar';
+import Home from './components/Home';
+import About from './components/About';
+import Skills from './components/Skills';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+
 
 function App() {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-slate-900 to-slate-700 flex items-center justify-center p-6">
-      <div className="bg-white text-gray-800 shadow-2xl rounded-2xl p-8 max-w-md w-full transform hover:scale-105 transition duration-300 ease-in-out">
-        <h1 className="text-3xl font-extrabold mb-4 text-center text-blue-600">Tailwind Test Successful 🎉</h1>
-        <p className="text-lg text-gray-600 mb-6 text-center">
-          If you can see this styled card, Tailwind is officially in the house.
-        </p>
-        <div className="flex justify-center">
-          <button className="bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-6 rounded-xl shadow-md transition">
-            I'm Working 😎
-          </button>
-        </div>
-      </div>
-    </div>
+    <ThemeProvider>
+      <Navbar/>
+      <Home/>
+      <About/>
+      <Skills/>
+      <Projects/>
+      <Contact/>
+      <Footer/>
+    </ThemeProvider>
   );
 }
 
